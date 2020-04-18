68cc25c245d536bc02c6abfd947729b09693a7d6
wnstj2007 <201802096@o.cnu.ac.kr>
2020-04-18 21:59:37 +0900

    코드 완성

--- soundEncode.py
+++ soundEncode.py
@@ -13,34 +13,28 @@ FEC_BYTES = 4
 
 def sound_generate(stream, freq):
     ######insert your code in function######
-    #t = np.arange(1)
-    #samples = np.sin(2 * np.pi * freq * t).astype(np.float32)
-    t = np.arange(len(freq))
-    samples = [np.sin(2*np.pi*HANDSHAKE_START_HZ*t)]
+    t = np.arange(44100*1.0)
+    samples = np.sin(2 * np.pi * freq * t / 44100).astype(np.float32)
+    stream.write(samples)
+'''
+    t = np.arange(44100*0.5)
+    samples = [np.sin(2*np.pi*HANDSHAKE_START_HZ*t/44100)]
     for i in range(len(freq)):
         print(freq[i])
         samples.append(np.sin(2*np.pi*freq[i]*t))
     samples.append(np.sin(2*np.pi*HANDSHAKE_END_HZ*t))
     samples = np.array(samples).astype(np.float32)
-
-    stream.write(samples)
+'''
 
 
 def divide_by_tone(each_data):
     ######insert your code in function######
-    data = []
-    data.append(each_data >> 4)
-    data.append(each_data & 15)
-    return data
+    pass
 
 
 def to_freq(step):
     ######insert your code in function######
-    freq = []
-    for i in step:
-        freq.append(START_HZ + (i*STEP_HZ))
-
-    return freq
+    return START_HZ + step * STEP_HZ
 
 def sound_code(fec_payload):
     p = pyaudio.PyAudio()
@@ -50,19 +44,19 @@ def sound_code(fec_payload):
                     output = True)
 
     ######insert your code below######
-    payload = []
-    for each_data in fec_payload:
-        payload.extend(divide_by_tone(each_data))
-    freqs = to_freq(payload)
-    sound_generate(stream, freqs)
-'''
+    data = []
+    for byte_data in fec_payload:
+        data.append(byte_data >> 4)
+        data.append(byte_data & 15)
+    freqs = []
+    for step in data:
+        freqs.append(to_freq(step))
     sound_generate(stream, HANDSHAKE_START_HZ)
-    print(freqs)
     for freq in freqs:
         print(freq)
         sound_generate(stream, freq)
     sound_generate(stream, HANDSHAKE_END_HZ)
-'''
+
 def play_sound(msg):
     byte_array = msg.encode()
     rs = RSCodec(FEC_BYTES)
